c41b16bc778b3c5cc701256735eebcb0ddb1dd70
Administrator <Administrator@PC021>
2019-03-27 15:57:32 +0700

    Add project files.

---
+++ DuyTung-BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DuyTung_BigSchool.MvcApplication" Language="C#" %>
---
+++ DuyTung-BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DuyTung_BigSchool.Startup))]
+namespace DuyTung_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
